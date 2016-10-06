b0e0dacc152442fd6e53d92c57539b78e3b00a1d
tuffery <furtuff@gmail.com>
2016-10-06 21:55:22 +0200

    relancement de l'application

---
+++ app/src/main/java/fr/wildcodeschool/apprenti/testouille/Cache.java
@@ -0,0 +1,46 @@
+package fr.wildcodeschool.apprenti.testouille;
+
+/**
+ * Created by Shooty31 on 06/10/2016.
+ */
+
+public class Cache {
+    private String hint;
+    private Float lat;
+    private Float lon;
+
+    public Cache(Float lat, Float lon,String hint) {
+        this.hint = hint;
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public void setHint(String hint) {
+        this.hint = hint;
+    }
+
+    public void setLat(Float lat) {
+        this.lat = lat;
+    }
+
+    public void setLon(Float lon) {
+        this.lon = lon;
+    }
+
+    public String getHint() {
+        return hint;
+    }
+
+
+    public Float getLat() {
+        return lat;
+    }
+
+    public Float getLon() {
+        return lon;
+    }
+}
+
+
+
+
---
+++ app/src/main/java/fr/wildcodeschool/apprenti/testouille/Constants.java
@@ -0,0 +1,13 @@
+package fr.wildcodeschool.apprenti.testouille;
+
+/**
+ * Created by tuffery on 06/10/16.
+ */
+
+public class Constants {
+
+
+  public static String IMG_URL = "IMG_URL";
+
+
+}
